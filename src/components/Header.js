3975226c76ff8ad31ea2f77df1a08e2bebad052a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-11-25 10:52:45 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
 
 function Header() {
+    const [user] = useAuthState(auth);
   return (
-    <div>
-      Header
-    </div>
+    <header className="flex items-center justify-between p-8 shadow-lg">
+      <h1 className="text-3xl font-bold">To-Do...or not To-Do</h1>
+      <img
+        src={user?.photoURL}
+        alt="profile"
+        className="h-10 w-10 rounded-full cursor-pointer"
+        onClick={() => auth.signOut()}
+      />
+    </header>
   );
 }
 
