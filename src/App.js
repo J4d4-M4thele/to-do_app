a28800e87ececd7c196360152863c80a88e5f2eb
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-11-25 10:00:42 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 
 function App() {
-  const user = false;
+  const [user] = useAuthState(auth);
 
   return (
     <div>
